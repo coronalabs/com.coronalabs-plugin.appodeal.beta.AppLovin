bd57b6010c2d3ec81e755ca41086dc95812670b3
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:53:15 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDAppLovinAdapter', },
+		frameworks = { 'AppLovinSDK', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
